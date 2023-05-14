314bdfea70e1ca35c939103570c9b407061c10a7
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-05-14 11:17:22 -0400

    update lesson reading

--- src/1_read_file.py
+++ src/1_read_file.py
@@ -1,12 +1,12 @@
 import boto3
 s3 = boto3.client('s3')
 
-# s3.list_buckets()
+s3.list_buckets()
 
-# s3.list_objects(Bucket='jigsaw-initial-bucket')
+s3.list_objects(Bucket='jigsaw-initial-bucket')
 
-# obj = s3.get_object(Bucket='jigsaw-initial-bucket', Key='yelp-lunch-nyc.csv')
+obj = s3.get_object(Bucket='jigsaw-initial-bucket', Key='yelp-lunch-nyc.csv')
 
+# obj['Body'].read()
 
-# bucket = s3client.create_bucket()
 
--- src/2_write_bucket.py
+++ src/2_write_bucket.py
@@ -2,13 +2,26 @@ import json
 import boto3
 
 s3 = boto3.client('s3')
-# response = s3.create_bucket(Bucket = 'jigsaw-sample-json')
-
-# json_object = {'hello': 'world'}
+# bucket = s3.create_bucket(Bucket = 'jigsaw-sample-json')
 
 # s3.upload_file('./yelp-lunch-nyc.csv', 'jigsaw-sample-json', 'lunch.csv')
 
 # obj = s3.get_object(Bucket='jigsaw-sample-json', Key='lunch.csv')
 
-# obj['Key'].read()
+# obj['Body'].read()
 
+
+
+# json_obj = {'hello': 'world'}
+
+# s3.put_object(
+#      Body=json.dumps(json_obj),
+#      Bucket='jigsaw-sample-json',
+#      Key='hello_world.json'
+# )
+
+# obj = s3.get_object(Bucket='jigsaw-sample-json', Key='hello_world.json')
+
+# text = obj['Body'].read()
+
+# hello_world_dict = json.loads(text)
